5d293825d26b19d3a837ec36ad1374ab8a78b937
bkhalf <bahaakhalf8@gmail.com>
2021-06-11 22:28:48 +0200

    Delete nonTerminal.cpp

--- Helper/nonTerminal.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "nonTerminal.h"
-
-nonTerminal::nonTerminal(string val,vector <string> term){
-    this->value=val;
-    this->terms=term;
-    this->eps=false;
-}
-#include "nonTerminal.h"
